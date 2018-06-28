d2d2e4f93a3a8f400ea622834ba365a0ab1aee76
Oakriggs <40615733+Oakriggs@users.noreply.github.com>
2018-06-28 16:16:41 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -6,5 +6,42 @@
 function makeGrid() {
 
 // Your code goes here!
+  console.log('makeGrid is ready');
 
+	var canvas, cell, gHeight, gWidth, rows;
+
+	canvas = $('#pixelCanvas');
+	gHeight = $('#inputHeight').val();
+	gWidth = $('#inputWeight').val();
+
+	canvas.children().remove();
+
+	for (i = 0; i < gHeight; i++) {
+		canvas.append('<tr></tr>');
+	};
+
+	rows = $('tr');
+
+	for (j = 0; j < gWidth; j++) {
+		rows.append('<td></td>');
+	};
+
+	cell = canvas.find('td');
+
+	cell.click(function(){
+		console.log("Colour Added");
+		var colour;
+		colour = $("#colorPicker").val();
+		$(this).attr('bgcolor', colour);
+	});
+};
+
+var submitProcess;
+
+submitProcess = $('input[type="submit"]');
+
+submitProcess.click(function(event){
+	event.preventDefault();
+	makeGrid();
+});
 }
